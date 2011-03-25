0fb5076aaba3a74a75df47f46767494cbd037af6
Enisher@gmail.com <Enisher@gmail.com@355be447-ce7d-4772-1abb-8f2928f63043>
2011-03-25 20:00:02 +0000

    + scratch of project

---
+++ trunk/src/test/java/datastorm/eventstore/otientdb/OrientEventStoreTest.java
@@ -0,0 +1,32 @@
+package datastorm.eventstore.otientdb;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test case for {@link OrientEventStore}.
+ *
+ * @author EniSh
+ */
+public class OrientEventStoreTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testAppendEvents() throws Exception {
+
+    }
+
+    @Test
+    public void testReadEvents() throws Exception {
+
+    }
+}
