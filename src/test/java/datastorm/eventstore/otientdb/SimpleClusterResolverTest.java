0ea7891225c2f6024e620a5e444c81ab7871e3fd
lomakin.andrey <lomakin.andrey@gmail.com>
2011-04-03 14:39:51 +0000

    Unit tests were added. Fixes issue #8.

--- src/test/java/datastorm/eventstore/otientdb/SimpleClusterResolverTest.java
+++ src/test/java/datastorm/eventstore/otientdb/SimpleClusterResolverTest.java
@@ -3,6 +3,11 @@ package datastorm.eventstore.otientdb;
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static datastorm.eventstore.otientdb.OrientEventStoreTestUtils.agId;
 import static org.junit.Assert.assertEquals;
@@ -15,9 +20,37 @@ import static org.mockito.Mockito.when;
  *
  * @author Andrey Lomakin
  */
+@RunWith(value = Parameterized.class)
 public class SimpleClusterResolverTest {
     private ODatabaseDocument database = mock(ODatabaseDocument.class);
     private SimpleClusterResolver clusterResolver = new SimpleClusterResolver("TestCluster");
+
+
+    private String clusterName;
+    private String typeName;
+    private String agId;
+
+    /**
+     * @return Cluster Name, Type Name, Aggregate Id
+     */
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+        return Arrays.asList(
+                new Object[]{
+                        "TestCluster", "Simple", "1"
+                },
+                new Object[]{
+                        "TestCluster", "Simple", "2"
+                }
+        );
+    }
+
+    public SimpleClusterResolverTest(String clusterName, String typeName, String agId) {
+        this.clusterName = clusterName;
+        this.typeName = typeName;
+        this.agId = agId;
+    }
+
 
     @Before
     public void setUp() {
@@ -27,22 +60,22 @@ public class SimpleClusterResolverTest {
 
     @Test
     public void testClusterCreation() {
-        when(database.getClusterIdByName("TestCluster")).thenReturn(-1);
+        when(database.getClusterIdByName(clusterName)).thenReturn(-1);
 
-        final String clusterName = clusterResolver.resolveClusterForAggregate("Simple", agId("1"));
+        final String resultClusterName = clusterResolver.resolveClusterForAggregate(typeName, agId(agId));
 
-        assertEquals("TestCluster", clusterName);
-        verify(database).getClusterIdByName("TestCluster");
-        verify(database).addPhysicalCluster("TestCluster", "TestCluster", -1);
+        assertEquals(clusterName, resultClusterName);
+        verify(database).getClusterIdByName(clusterName);
+        verify(database).addPhysicalCluster(clusterName, clusterName, -1);
     }
 
     @Test
     public void testClusterResolution() {
-        when(database.getClusterIdByName("TestCluster")).thenReturn(1);
+        when(database.getClusterIdByName(clusterName)).thenReturn(1);
 
-        final String clusterName = clusterResolver.resolveClusterForAggregate("Simple", agId("1"));
+        final String resultClusterName = clusterResolver.resolveClusterForAggregate(typeName, agId(agId));
 
-        assertEquals("TestCluster", clusterName);
-        verify(database).getClusterIdByName("TestCluster");
+        assertEquals(clusterName, resultClusterName);
+        verify(database).getClusterIdByName(clusterName);
     }
 }
