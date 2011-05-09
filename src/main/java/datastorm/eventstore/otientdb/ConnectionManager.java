a058fee2db54d836a2735222cfb3637720be48cb
enisher <Enisher@gmail.com>
2011-05-09 13:32:15 +0000

    Issue #17. Add TransactionSynchronization support and IntegrationTest for transaction manager. Static proxy for oDatabaseDocument has been deleted.

---
+++ src/main/java/datastorm/eventstore/otientdb/ConnectionManager.java
@@ -0,0 +1,33 @@
+package datastorm.eventstore.otientdb;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentPool;
+
+/**
+ * @author EniSh
+ *         Date: 23.04.11
+ */
+public class ConnectionManager {
+    private final String databaseURL;
+    private final String databaseUserName;
+    private final String databasePassword;
+    private final ODatabaseDocumentPool globalDatabasePool = ODatabaseDocumentPool.global();
+
+    public ConnectionManager(final String databaseURL, final String databaseUserName, final String databasePassword) {
+        this.databaseURL = databaseURL;
+        this.databaseUserName = databaseUserName;
+        this.databasePassword = databasePassword;
+    }
+
+    public ConnectionManager(final String databaseURL, final String databaseUserName, final String databasePassword,
+                             final int minPoolSize, final int maxPoolSize) {
+        this.databaseURL = databaseURL;
+        this.databaseUserName = databaseUserName;
+        this.databasePassword = databasePassword;
+        globalDatabasePool.setup(minPoolSize, maxPoolSize);
+    }
+
+    public ODatabaseDocument getNewConnection() {
+        return globalDatabasePool.acquire(databaseURL, databaseUserName, databasePassword);
+    }
+}
