d6081cbc0d58c440ae82061226822a90266e7102
enisher <Enisher@gmail.com>
2011-05-15 20:43:37 +0000

    Issue #17. Refactor ConnectionManager

---
+++ src/test/java/ua/com/datastorm/eventstore/orientdb/ConnectionManagerTest.java
@@ -0,0 +1,42 @@
+package ua.com.datastorm.eventstore.orientdb;
+
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentPool;
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author EniSh
+ *         Date: 15.05.11
+ */
+public class ConnectionManagerTest {
+    private ConnectionManager connectionManager;
+    private ODatabaseDocumentTx expectedConnection = mock(ODatabaseDocumentTx.class);
+    private String expectedDatabaseUrl = "databaseUrl";
+    private String expectedUserName = "userName";
+    private String expectedPassword = "databasePass";
+    private int expectedMinPoolSize = 10;
+    private int expectedMaxPoolSize = 50;
+
+    @Test
+    public void testGetNewConnection() {
+        ODatabaseDocumentPool poolMock = mock(ODatabaseDocumentPool.class);
+
+        doNothing().when(poolMock).setup(expectedMinPoolSize, expectedMaxPoolSize);
+
+        when(poolMock.acquire(expectedDatabaseUrl, expectedUserName, expectedPassword)).thenReturn(expectedConnection);
+
+        connectionManager = new ConnectionManager(expectedDatabaseUrl, expectedUserName, expectedPassword, expectedMinPoolSize, expectedMaxPoolSize, poolMock);
+
+        ODatabaseDocument newConnection = connectionManager.getNewConnection();
+
+        assertEquals("Created connection not same as expected.", expectedConnection, newConnection);
+
+        verify(poolMock).setup(expectedMinPoolSize, expectedMaxPoolSize);
+        verify(poolMock).acquire(expectedDatabaseUrl, expectedUserName, expectedPassword);
+        verifyZeroInteractions(expectedConnection);
+    }
+}
